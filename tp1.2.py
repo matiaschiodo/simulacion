3620b3045925f8892cc02fead9e0b75ce8a1603f
Julian Giovanelli <julian@viirtue.com>
2024-04-29 22:18:49 -0300

    Algoritmos de estrategia completos

--- tp1.2.py
+++ tp1.2.py
@@ -43,24 +43,56 @@ def girarRuleta(evento,apuesta,capital):
             gano = False
         return tirada, capital, gano
 
-def martingala(apuesta_inicial, capital_maximo, cantidad_de_apuestas):
+
+def martingala(gano, apuesta, apuesta_inicial):
+    return apuesta_inicial if gano else apuesta * 2
+
+def dalembert(gano, apuesta, apuesta_inicial):
+    # Nosotros tomamos que la unidad en la estrategia es la apuesta inicial
+    return max(apuesta_inicial, apuesta - apuesta_inicial) if gano else apuesta + apuesta_inicial
+
+def fibonacci(gano, apuesta_inicial, indice_fibonacci):
+    # Nosotros tomamos que la unidad en la estrategia es la apuesta inicial
+    fib = lambda n:pow(2<<n,n+1,(4<<2*n)-(2<<n)-1)%(2<<n)
+
+    if gano:
+        indice_fibonacci = max(1 ,indice_fibonacci - 1)  
+    else : 
+        indice_fibonacci +=1
+
+    apuesta = apuesta_inicial * fib(indice_fibonacci)
+
+    return apuesta, indice_fibonacci
+
+def constante(apuesta_inicial):
+    return apuesta_inicial
+
+def martingala_inversa(gano, apuesta, apuesta_inicial):
+    return apuesta * 2 if gano else apuesta_inicial
+
+
+def apostar(apuesta_inicial, capital_maximo, cantidad_de_apuestas, estrategia):
     capital = capital_maximo
     apuesta = apuesta_inicial
     apuesta_numero = 0
+    indice_fib = 1
 
     while capital > apuesta and apuesta_numero < cantidad_de_apuestas:
         apuesta_numero += 1
 
         tirada, capital, gano = girarRuleta("Rojo", apuesta, capital)
+        
+        print(f"{'Gano' if gano else 'Perdio'}, capital actual: {capital} - Apuesta Nro: {apuesta_numero} - Aposto {apuesta}")
 
-        if gano:
-            print(f"Gano, capital actual: {capital} - Apuesta Nro: {apuesta_numero} - Aposto {apuesta}")
-            apuesta = apuesta_inicial
-        else:
-            apuesta *= 2
-            print(f"Perdio, capital actual: {capital} - Apuesta Nro: {apuesta_numero} - Aposto {apuesta}")
+        if estrategia == "M": apuesta = martingala(gano, apuesta, apuesta_inicial)
+        if estrategia == "D": apuesta = dalembert(gano, apuesta, apuesta_inicial)
+        if estrategia == "F": apuesta, indice_fib = fibonacci(gano, apuesta_inicial, indice_fib)
+        if estrategia == "C": apuesta = constante(apuesta_inicial)
+        if estrategia == "MI": apuesta = martingala_inversa(gano, apuesta, apuesta_inicial)
 
     return capital, apuesta_numero
+
+
 
 # Verificar si se proporciona el número de valores como argumento
 #if len(sys.argv) != 7 or sys.argv[1] != "-t" or sys.argv[3] != "-c" or sys.argv[5] != "-e":
@@ -74,7 +106,10 @@ numero_elegido = 10 #int(sys.argv[6])
 apuesta_inicial = 10
 capital_maximo = 1000
 numero_maximo = 1000
+estrategia = "C"
 
-capital_final, numero_jugadas = martingala(apuesta_inicial, capital_maximo, numero_maximo)
+capital_final, numero_jugadas = apostar(apuesta_inicial, capital_maximo, numero_maximo, estrategia)
 print(f"Capital final: {capital_final}")
 print(f"Número de jugadas: {numero_jugadas}")
+
+# TODO: Informe, setear valores iniciales por consola y capital ilimitado 
