47bbffb064c5819f180981395f139563c6e1cf79
Emiliano Rios <erios@comparaonline.com>
2024-05-15 22:29:17 -0300

    added new grahps tp1.2

--- tp1.2.py
+++ tp1.2.py
@@ -4,40 +4,102 @@ import matplotlib.pyplot as plt
 import sys
 
 
-def graficar_frec(frecuencias):
+def graficar_frecuencia(frecuencias):
     plt.bar(range(len(frecuencias)), frecuencias, align='center', alpha=0.5)
-    plt.xlabel('Iteración')
-    plt.ylabel('Frecuencia de Ganar')
-    plt.title('Frecuencia de Ganar en Cada Iteración')
+    plt.axhline(y=0.486, color="black", linestyle='--', label="Frecuencia esperada")
+    plt.xlabel('Tirada')
+    plt.title('Frecuencia de Ganar por tirada')
+    plt.legend(loc='upper right')
     plt.show()
 
 
-def graficar_estrategia(capital, capital_inicial):
-    plt.plot(capital)
-    plt.xlabel('Numero de Apuesta')
-    plt.ylabel('Capital')
-    plt.title('Martingala Evolucion')
-    plt.axhline(y=capital_inicial, color="red", linestyle='--')  # promedio esperado
+def graficar_capital(capitales, capital_inicial):
+    plt.plot(capitales)
+    plt.xlabel('Tirada')
+    plt.title('Evolución de capital por tirada')
+    plt.axhline(y=capital_inicial, color="red", linestyle='--', label="Capital Inicial")
+    plt.legend(loc='upper right')
     plt.show()
 
-def graficar_multiples_corridas(capitales, capital_inicial):
+def graficar_capitales(capitales, capital_inicial):
     for i in capitales:
         plt.plot(i)
     plt.xlabel('Numero de Apuesta')
     plt.ylabel('Capital')
-    plt.title('Martingala Evolucion')
-    plt.axhline(y=capital_inicial, color="red", linestyle='--')  # promedio esperado
+    plt.title('Evolución de capital de múltiples corridas por tirada')
+    plt.axhline(y=capital_inicial, color="red", linestyle='--', label="Capital Inicial")
+    plt.legend(loc='upper right')
     plt.show()
 
 
-def graficar_promedio_estrategia(promedio_estrategia, capital_inicial):
-    plt.title("Promedio de capital estrategia X")
-    plt.plot(promedio_estrategia)
-    plt.xlabel('Numero de Apuesta')
-    plt.ylabel('Capital')
-    plt.axhline(y=capital_inicial, color="red", linestyle='--')
+def grafica_porcentaje_ganancias(porcentaje_ganancias_acumulado):
+    plt.figure(figsize=(10, 6))
+    plt.plot(range(1, len(porcentaje_ganancias_acumulado) + 1), porcentaje_ganancias_acumulado, marker='o')
+    plt.xlabel('Número de Corridas')
+    plt.ylabel('Porcentaje de Corridas con Ganancias (%)')
+    plt.title('Porcentaje de Corridas con Ganancias Acumulado')
+    plt.grid(True)
     plt.show()
 
+
+def apostar(cant_corridas, apuesta_inicial, capital_inicial, cantidad_de_apuestas, estrategia, capital_ilimitado):
+
+    capitales = []
+    ganadas_bool = []
+    frecuencias = []
+    esta_en_ganancia_bool = []
+    for i in range(cant_corridas):
+        capital = capital_inicial
+        apuesta = apuesta_inicial
+        apuesta_numero = 0
+        indice_fib = 1
+        capitales.append([])
+        ganadas_bool.append([])
+        frecuencias.append([])
+        esta_en_ganancia_bool.append([])
+
+        while apuesta_numero < cantidad_de_apuestas:
+            if (capital > apuesta or capital_ilimitado):
+                tirada, capital, gano = girarRuleta("Rojo", apuesta, capital)
+                print(f"{apuesta_numero}. Capital: {capital} - Apostó: {apuesta}")
+
+                if estrategia == "M": apuesta = martingala(gano, apuesta, apuesta_inicial)
+                if estrategia == "D": apuesta = dalembert(gano, apuesta, apuesta_inicial)
+                if estrategia == "F": apuesta, indice_fib = fibonacci(gano, apuesta_inicial, indice_fib)
+                if estrategia == "C": apuesta = constante(apuesta_inicial)
+                if estrategia == "MI": apuesta = martingala_inversa(gano, apuesta, apuesta_inicial)
+                ganadas_bool[i].append(gano)
+                frecuencias[i].append(ganadas_bool[i].count(True) / len(ganadas_bool[i]))
+                ultima_frecuencia = ganadas_bool[i].count(True) / len(ganadas_bool[i])
+            else:
+                frecuencias[i].append(ultima_frecuencia)
+
+            esta_en_ganancia_bool[i].append(capital > capital_inicial)
+            capitales[i].append(capital)
+
+            if capital < apuesta and not capital_ilimitado: print(
+                f"La próxima apuesta ({apuesta}) es superior al capital ({capital}).")
+            apuesta_numero += 1
+            if apuesta_numero == cantidad_de_apuestas: print(f"La simulación llegó a {cantidad_de_apuestas} apuestas.")
+
+
+    porcentaje_ganancias_acumulado = []
+    corridas_con_ganancias = 0
+    for i, corrida in enumerate(capitales, 1):
+        if corrida[cantidad_de_apuestas - 1] > capital_inicial:
+            corridas_con_ganancias += 1
+        porcentaje_ganancias_acumulado.append((corridas_con_ganancias / i))
+
+    graficar_capital(capitales[0], capital_inicial)
+    graficar_frecuencia(frecuencias[0])
+    graficar_capitales(capitales, capital_inicial)
+    grafica_porcentaje_ganancias(porcentaje_ganancias_acumulado)
+
+    # Calcular el porcentaje de veces que la estrategia resultó en una ganancia o pérdida en cada corrida
+    #porcentaje_ganancias_acumulado = []
+    #corridas_con_ganancias = 0
+
+    print('uwu')
 
 def girarRuleta(evento, apuesta, capital):
     rojo = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
@@ -113,92 +175,21 @@ def martingala_inversa(gano, apuesta, apuesta_inicial):
     return apuesta * 2 if gano else apuesta_inicial
 
 
-def apostar(cant_corridas, apuesta_inicial, capital_inicial, cantidad_de_apuestas, estrategia, capital_ilimitado):
-    #promedio_estrategia = np.zeros(cantidad_de_apuestas)
-    #frecuencias_historico = []
-    #capital_historico = []
-    #ganadas_historico = []
+# Verificar si se proporciona el número de valores como argumento
+if len(sys.argv) != 11 or sys.argv[1] != "-t" or sys.argv[3] != "-c" or sys.argv[5] != "-s" or sys.argv[7] != "-a":
+    print("Uso: python tp1.2.py -t <tiradas> -c <corridas> -s <estrategia> -a <tipo de capital>.")
+    print("Estrategias: M (Martingala), D (D'Alembert), F (Fibonacci), MI (Martingala Inversa), C (Constante)")
+    print("Tipo de capital: I (Infinito), L (Limitado)")
+    sys.exit(1)
 
-    capitales = []
-    ganadas_bool = []
-    frecuencias = []
-
-    for i in range(cant_corridas):
-        capital = capital_inicial
-        apuesta = apuesta_inicial
-        apuesta_numero = 0
-        indice_fib = 1
-        capitales.append([])
-        ganadas_bool.append([])
-        frecuencias.append([])
-
-        while apuesta_numero < cantidad_de_apuestas:
-            if (capital > apuesta or capital_ilimitado):
-                tirada, capital, gano = girarRuleta("Rojo", apuesta, capital)
-                print(f"{apuesta_numero}. Capital: {capital} - Apostó: {apuesta}")
-
-                if estrategia == "M": apuesta = martingala(gano, apuesta, apuesta_inicial)
-                if estrategia == "D": apuesta = dalembert(gano, apuesta, apuesta_inicial)
-                if estrategia == "F": apuesta, indice_fib = fibonacci(gano, apuesta_inicial, indice_fib)
-                if estrategia == "C": apuesta = constante(apuesta_inicial)
-                if estrategia == "MI": apuesta = martingala_inversa(gano, apuesta, apuesta_inicial)
-                ganadas_bool[i].append(gano)
-                frecuencias[i].append(ganadas_bool[i].count(True) / len(ganadas_bool[i]))
-                ultima_frecuencia = ganadas_bool[i].count(True) / len(ganadas_bool[i])
-            else:
-                frecuencias[i].append(ultima_frecuencia)
-
-            capitales[i].append(capital)
-
-            #capital_historico.append(capital)
-            #ganadas_historico.append(gano)
-            #frecuencias_historico.append(ganadas_historico.count(True) / len(ganadas_historico))
-            print(capitales[i])
-            print(ganadas_bool[i])
-            print(frecuencias[i])
-
-            if capital < apuesta and not capital_ilimitado: print(
-                f"La próxima apuesta ({apuesta}) es superior al capital ({capital}).")
-            apuesta_numero += 1
-            if apuesta_numero == cantidad_de_apuestas: print(f"La simulación llegó a {cantidad_de_apuestas} apuestas.")
-    capitales_transpuesta = [list(value) for value in zip(*capitales)]
-    promedio_capital = [np.average(value) for value in capitales_transpuesta]
-    frecuencias_transpuesta = [list(value) for value in zip(*frecuencias)]
-    promedio_frecuencia = [np.average(value) for value in frecuencias_transpuesta]
-    #graficar_promedio_estrategia(promedio_capital, capital_inicial)
-    #graficar_frec(promedio_frecuencia)
-    graficar_multiples_corridas(capitales,capital_inicial)
-    print('uwu')
-
-        #for i in range(cantidad_de_apuestas):
-        #    promedio_estrategia[i] += capital_historico[i]  # acumulo el capital en cada tirada
-
-        #if cant_corridas == 1:
-        #    graficar_estrategia(capital_historico, capital_inicial)
-         #   graficar_frec(frecuencias_historico)
-
-    #for i in range(cantidad_de_apuestas):
-     #   promedio_estrategia[i] /= cant_corridas  # calculo el promedio del capital en cada tirada
-
-    #if cant_corridas > 1:
-    #    graficar_promedio_estrategia(promedio_estrategia, capital_inicial)
-
-
-# # Verificar si se proporciona el número de valores como argumento
-# if len(sys.argv) != 11 or sys.argv[1] != "-t" or sys.argv[3] != "-c" or sys.argv[5] != "-e" or sys.argv[7] != "-s" or sys.argv[9] != "-a":
-#    print("Uso: python tp1.2.py -t <tiradas> -c <corridas> -e <numero elegido> -s <estrategia> -a <tipo de capital>.")
-#    sys.exit(1)
-
-cant_tiradas = int(100)  # int(sys.argv[2])
-cant_corridas = int(50)  # int(sys.argv[4])
-# numero_elegido = int(sys.argv[6])
-estrategia = "D"  # sys.argv[8].upper()
-capital_ilimitado = True  # sys.argv[10] == 'i'
+cant_tiradas = int(sys.argv[2])
+cant_corridas = int(sys.argv[4])
+estrategia = sys.argv[8].upper()
+capital_ilimitado = sys.argv[10].upper() == 'I'
 
 capital_inicial = 500
 apuesta_inicial = 10
-# numero_maximo = 100
 
-# print(f"Corridas: {cant_corridas} - Tiradas: {cant_tiradas} - Numero: {numero_elegido} - Estrategia: {estrategia} - Capital: {'Ilimitado' if capital_ilimitado else capital_inicial}")
+print(f"Corridas: {cant_corridas} - Tiradas: {cant_tiradas} - Estrategia: {estrategia} - Capital: {'Ilimitado' if capital_ilimitado else capital_inicial}")
 
 apostar(cant_corridas, apuesta_inicial, capital_inicial, cant_tiradas, estrategia, capital_ilimitado)
